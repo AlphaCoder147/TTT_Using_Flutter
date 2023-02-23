8eafe7a2c6b71f43047c61aa00aeaed9ebc80dff
AlphaCoder147 <thescholar8101@gmail.com>
2023-02-23 19:59:01 +0530

    Third

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,6 +8,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool ohturn = true;
   List<String> displayEx0h = ['', '', '', '', '', '', '', '', ''];
   @override
   Widget build(BuildContext context) {
@@ -39,7 +40,74 @@ class _HomePageState extends State<HomePage> {
 
   void _tapped(int index) {
     setState(() {
-      displayEx0h[index] = 'o';
+      if (ohturn) {
+        displayEx0h[index] = 'o';
+      } else {
+        displayEx0h[index] = 'x';
+      }
+      ohturn = !ohturn;
+      _checkwinner();
     });
+  }
+
+  void _checkwinner() {
+    //row 1
+    if (displayEx0h[0] == displayEx0h[1] &&
+        displayEx0h[0] == displayEx0h[2] &&
+        displayEx0h[0] != '') {
+      _showwindialog();
+    }
+    //row 2
+    if (displayEx0h[3] == displayEx0h[4] &&
+        displayEx0h[3] == displayEx0h[5] &&
+        displayEx0h[3] != '') {
+      _showwindialog();
+    }
+    //row 3
+    if (displayEx0h[6] == displayEx0h[7] &&
+        displayEx0h[6] == displayEx0h[8] &&
+        displayEx0h[6] != '') {
+      _showwindialog();
+    }
+    //column 1
+    if (displayEx0h[0] == displayEx0h[3] &&
+        displayEx0h[0] == displayEx0h[6] &&
+        displayEx0h[0] != '') {
+      _showwindialog();
+    }
+    //column 2
+    if (displayEx0h[1] == displayEx0h[4] &&
+        displayEx0h[1] == displayEx0h[7] &&
+        displayEx0h[1] != '') {
+      _showwindialog();
+    }
+    //column 3
+    if (displayEx0h[2] == displayEx0h[5] &&
+        displayEx0h[2] == displayEx0h[8] &&
+        displayEx0h[2] != '') {
+      _showwindialog();
+    }
+    //diagonal 1
+    if (displayEx0h[0] == displayEx0h[4] &&
+        displayEx0h[0] == displayEx0h[8] &&
+        displayEx0h[0] != '') {
+      _showwindialog();
+    }
+    //diagonal 2
+    if (displayEx0h[2] == displayEx0h[4] &&
+        displayEx0h[2] == displayEx0h[6] &&
+        displayEx0h[2] != '') {
+      _showwindialog();
+    }
+  }
+
+  void _showwindialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return const AlertDialog(
+            title: Text("WINNER"),
+          );
+        });
   }
 }
