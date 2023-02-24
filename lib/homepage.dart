e6f4404e12ec3b7197dc9e709b69435869abd9ec
AlphaCoder147 <thescholar8101@gmail.com>
2023-02-24 20:13:59 +0530

    Seventh

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,79 +15,88 @@ class _HomePageState extends State<HomePage> {
   var myTextStyle2 = const TextStyle(color: Colors.white, fontSize: 20);
   int exscore = 0;
   int ohscore = 0;
+  int filled = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[800],
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(30.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Player X',
-                          style: myTextStyle,
-                        ),
-                        Text(exscore.toString(), style: myTextStyle),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(30.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Player O',
-                          style: myTextStyle,
-                        ),
-                        Text(
-                          ohscore.toString(),
-                          style: myTextStyle,
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: GridView.builder(
-                itemCount: 9,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3),
-                itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                    onTap: () {
-                      _tapped(index);
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(color: (Colors.grey[700])!)),
-                      child: Center(
-                        child: Text(
-                          displayEx0h[index],
-                          style: const TextStyle(
-                              color: Colors.white, fontSize: 40),
-                        ),
+    return SingleChildScrollView(
+      child: Scaffold(
+        backgroundColor: Colors.grey[800],
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Player X',
+                            style: myTextStyle,
+                          ),
+                          Text(exscore.toString(), style: myTextStyle),
+                        ],
                       ),
                     ),
-                  );
-                }),
-          ),
-          Expanded(
-            child: Container(),
-          ),
-        ],
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Player O',
+                            style: myTextStyle,
+                          ),
+                          Text(
+                            ohscore.toString(),
+                            style: myTextStyle,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: GridView.builder(
+                  itemCount: 9,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        _tapped(index);
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: (Colors.grey[700])!)),
+                        child: Center(
+                          child: Text(
+                            displayEx0h[index],
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 40),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            Expanded(
+              child: Container(
+                child: Column(children: [
+                  const SizedBox(height: 10),
+                  Text('TIC TAC TOE', style: myTextStyle),
+                  Text('@ALPHAPANTHERKILO', style: myTextStyle),
+                ]),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -95,11 +105,16 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       if (ohturn && displayEx0h[index] == '') {
         displayEx0h[index] = 'o';
+        filled++;
       } else if (!ohturn && displayEx0h[index] == '') {
         displayEx0h[index] = 'x';
+        filled++;
       }
       ohturn = !ohturn;
-      _checkwinner();
+
+      Timer(const Duration(milliseconds: 500), () {
+        _checkwinner();
+      });
     });
   }
 
@@ -151,7 +166,30 @@ class _HomePageState extends State<HomePage> {
         displayEx0h[2] == displayEx0h[6] &&
         displayEx0h[2] != '') {
       _showwindialog(displayEx0h[2]);
+    } else if (filled == 9) {
+      _showDrawdialog();
     }
+  }
+
+  void _showDrawdialog() {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('DRAW'),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  _clear();
+                  Navigator.of(context).pop();
+                },
+                child: Text("PLAY AGAIN!", style: myTextStyle2),
+              )
+            ],
+          );
+        });
+    _clear();
   }
 
   void _showwindialog(String winner) {
@@ -187,5 +225,6 @@ class _HomePageState extends State<HomePage> {
         displayEx0h[i] = '';
       }
     });
+    filled = 0;
   }
 }
